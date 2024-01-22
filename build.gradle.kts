f0c1874fe9c9ae07bd2b5c0bede187277b9ce474
Donam Kim <donam.kim@gi-vita.io>
2024-01-22 16:53:04 +0900

    add javafx dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.22"
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "dev.donam"
@@ -13,6 +14,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.7.3")
 
     testImplementation(kotlin("test"))
 }
@@ -23,4 +25,9 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
+}
+
+javafx {
+    version = "21.0.2"
+    modules = listOf("javafx.controls")
 }
