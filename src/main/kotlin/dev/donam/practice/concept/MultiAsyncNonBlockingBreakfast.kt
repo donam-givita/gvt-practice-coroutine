4cadc246f29c15e09c2e0b0871b8e678578eac17
minseop.lee <minseop.lee@gi-vita.io>
2024-01-24 08:29:21 +0900

    feat: two coroutineScope

--- src/main/kotlin/dev/donam/practice/concept/MultiAsyncNonBlockingBreakfast.kt
+++ src/main/kotlin/dev/donam/practice/concept/MultiAsyncNonBlockingBreakfast.kt
@@ -2,13 +2,16 @@ package dev.donam.practice.concept
 
 import dev.donam.practice.concept.BreakfastAsync.applyButter
 import dev.donam.practice.concept.BreakfastAsync.applyJam
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.selects.select
 import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors
 import kotlin.system.measureTimeMillis
@@ -17,44 +20,50 @@ fun main() = runBlocking {
     val time = measureTimeMillis {
         println("========== fun main: ${Thread.currentThread().name} ==========")
 
-        Executors.newFixedThreadPool(2).asCoroutineDispatcher().use { dispatcher ->
-            withContext(dispatcher) {
-                // pourCoffee
-                async(coroutineContext) {
-                    val cupOfCoffee: Coffee = BreakfastAsync.pourCoffee()
-                    println("> coffee is ready: $cupOfCoffee")
-                }
+        val deferred1 = CoroutineScope(Dispatchers.IO).async {
+            val deferredTime = measureTimeMillis {
+                val coffeeDeferredItem = async { BreakfastAsync.pourCoffee() }
+                val eggsDeferred = async { BreakfastAsync.fryEggs(2) }
+                val baconDeferred = async { BreakfastAsync.fryBacon(3) }
 
-                // fryEggs
-                async(coroutineContext) {
-                    val eggs: Egg = BreakfastAsync.fryEggs(2)
-                    println("> eggs are ready: $eggs")
-                }
+                val response = awaitAll(coffeeDeferredItem, eggsDeferred, baconDeferred)
 
-                // fryBacon
-                async(coroutineContext) {
-                    val bacon: Bacon = BreakfastAsync.fryBacon(3)
-                    println("> bacon is ready: $bacon")
-                }
+                println("> coffee is ready: ${response[0]}")
+                println("> eggs are ready: ${response[1]}")
+                println("> bacon is ready: ${response[2]}")
+            }
 
-                // toastBread
-                // applyButter
-                // applyJam
-                async {
-                    val toast: Toast = BreakfastAsync.toastBread(2)
+            deferredTime
+        }
+
+        val deferred2 = CoroutineScope(Dispatchers.IO).async {
+            val deferredTime = measureTimeMillis {
+                val toastDeferred = async {
+                    BreakfastAsync.toastBread(2)
                         .applyButter()
                         .applyJam()
-                    println("> toast is ready: $toast")
                 }
 
-                // pourOrangeJuice
-                async {
-                    val cupOfOrangeJuice: Juice = BreakfastAsync.pourOrangeJuice()
-                    println("> orange juice is ready: $cupOfOrangeJuice")
-                }
+                val cupOfOrangeJuiceDeferred = async { BreakfastAsync.pourOrangeJuice() }
+
+                val response = awaitAll(toastDeferred, cupOfOrangeJuiceDeferred)
+
+                println("> toast is ready: ${response[0]}")
+                println("> orange juice is ready: ${response[1]}")
             }
+
+            deferredTime
         }
+
+        val response = select {
+            deferred1.onAwait { "win deferred1 took ${it}ms" }
+            deferred2.onAwait { "win deferred2 took ${it}ms " }
+        }.also {
+            coroutineContext.cancelChildren()
+        }
+        println("Select: $response")
     }
+
     println("> MultiAsyncNonBlockingBreakfast is ready!")
     println("\n\n>>> took ${time}ms\n\n")
 }
